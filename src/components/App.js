8e80d5b15dfc6da53ac1da68bdc1e045fb12003b
fatouuuu <fatmaabdullrahim@gmail.com>
2023-01-11 08:52:16 +0300

    completed lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+// parent component w poster omitted uses default value from the moviecard prop key
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
+      {/* posterSrc is omitted, so the default value will be used instead */}
       <MovieCard title={title} genres={genresArr} />
     </div>
   );
 }
+/* child component
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
+    </div>
+  );
+} */
+
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+// Without Destructuring
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
 }) {
   return (
     <div className="movie-card">
@@ -13,5 +14,17 @@ function MovieCard({
     </div>
   );
 }
+
+/* With Destructuring
+function MovieCard({ title, posterSrc, genres }) {
+  return (
+    <div className="movie-card">
+      <img src={posterSrc} alt={title} />
+      <h2>{title}</h2>
+      <small>{genres.join(", ")}</small>
+    </div>
+  );
+}
+*/
 
 export default MovieCard;
